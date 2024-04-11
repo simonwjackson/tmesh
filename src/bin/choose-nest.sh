7e80d20e6232dbbf72e811d25e7d8664cb95fd41
Simon W. Jackson <hello@simonwjackson.io>
2024-04-11 18:07:21 -0500

    fix: obtain host list from config

--- src/bin/choose-nest.sh
+++ src/bin/choose-nest.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+TMESH_ALL_SERVERS="$(yq -e -oy '.hosts | .[]' "${TMESH_CONFIG}" 2>/dev/null || " ")"
+
 server_selection=$(
-  echo -e "$TMUX_ALL_SERVERS" |
+  echo -e "$TMESH_ALL_SERVERS" |
     fzf \
       --delimiter='\n' \
       --bind 'ctrl-c:abort'
