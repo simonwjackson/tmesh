5d592cac6595536217f26e2b7e17ac6675ff157d
Simon W. Jackson <simon.jackson@gmail.com>
2024-04-11 19:47:09 -0500

    fix: remove empty space in server list command

--- src/bin/choose-nest.sh
+++ src/bin/choose-nest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TMESH_ALL_SERVERS="$(yq -e -oy '.hosts | .[]' "${TMESH_CONFIG}" 2>/dev/null || " ")"
+TMESH_ALL_SERVERS="$(yq -e -oy '.hosts | .[]' "${TMESH_CONFIG}" 2>/dev/null)"
 
 server_selection=$(
   echo -e "$TMESH_ALL_SERVERS" |
