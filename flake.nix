80144b560a4ac3621361f5cb4359571c9fd96595
Simon W. Jackson <github@simonwjackson.io>
2025-12-15 18:19:54 -0700

    fix(flake): use external module definitions instead of inline

--- flake.nix
+++ flake.nix
@@ -38,118 +38,12 @@
     });
 
     nixosModules = {
-      tmesh = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        inherit (pkgs.stdenv.hostPlatform) system;
-        pname = "tmesh";
-        package = self.packages.${system}.${pname};
-        cfg = config.programs.${pname};
-      in {
-        options.programs.${pname} = {
-          enable = lib.mkEnableOption "${pname}";
-
-          settings = lib.mkOption {
-            type = lib.types.attrs;
-            default = {};
-            description = "Configuration settings.";
-          };
-
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = package;
-            description = "The package to use for ${pname}.";
-          };
-
-          tmeshServerTmuxConfig = lib.mkOption {
-            type = lib.types.lines;
-            default = "";
-            description = "Tmux configuration for tmesh server.";
-          };
-
-          tmeshTmuxConfig = lib.mkOption {
-            type = lib.types.lines;
-            default = "";
-            description = "Tmux configuration for tmesh.";
-          };
-        };
-
-        config = lib.mkIf cfg.enable {
-          environment.etc."${pname}/tmesh-server.tmux.conf" = {
-            text = cfg.tmeshServerTmuxConfig;
-            mode = "0644";
-          };
-
-          environment.etc."${pname}/tmesh.tmux.conf" = {
-            text = cfg.tmeshTmuxConfig;
-            mode = "0644";
-          };
-
-          environment.systemPackages = [
-            cfg.package
-          ];
-        };
-      };
+      tmesh = import ./modules/nixos/default { inherit self; };
       default = self.nixosModules.tmesh;
     };
 
     homeManagerModules = {
-      tmesh = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        inherit (pkgs.stdenv.hostPlatform) system;
-        pname = "tmesh";
-        package = self.packages.${system}.${pname};
-        cfg = config.programs.${pname};
-      in {
-        options.programs.${pname} = {
-          enable = lib.mkEnableOption "${pname}";
-
-          settings = lib.mkOption {
-            type = lib.types.attrs;
-            default = {};
-            description = "Configuration settings.";
-          };
-
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = package;
-            description = "The package to use for ${pname}.";
-          };
-
-          tmeshServerTmuxConfig = lib.mkOption {
-            type = lib.types.lines;
-            default = "";
-            description = "Tmux configuration for tmesh server.";
-          };
-
-          tmeshTmuxConfig = lib.mkOption {
-            type = lib.types.lines;
-            default = "";
-            description = "Tmux configuration for tmesh.";
-          };
-        };
-
-        config = lib.mkIf cfg.enable {
-          xdg.configFile."${pname}/tmesh-server.tmux.conf" = {
-            text = cfg.tmeshServerTmuxConfig;
-          };
-
-          xdg.configFile."${pname}/tmesh.tmux.conf" = {
-            text = cfg.tmeshTmuxConfig;
-          };
-
-          home.packages = [
-            cfg.package
-          ];
-        };
-      };
+      tmesh = import ./modules/home-manager { inherit self; };
       default = self.homeManagerModules.tmesh;
     };
 
